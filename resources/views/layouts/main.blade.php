8fa8bb6efd041ac6c6d2079968947afb0a0b2284
Nazarii1488 <raider.bike@kr.net>
2019-10-24 20:45:04 +0300

    my commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="/css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body class="d-flex flex-column h-100">
+    @include('layouts.header')
+    @include('layouts.slider')
+    @yield('content')
+    @include('layouts.footer')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/js/bootstrap.js"></script>
+    </body>
+</html>
